8673de23e94012f95ddc46f6536da48c2c6e25b9
eathan <eathandueno@gmail.com>
2023-04-29 16:06:16 -0600

    Created a task list and handling functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react';
 function App() {
+  let [taskList,setTaskList] = useState([]);
+  function handleSubmit(event){
+    event.preventDefault();
+    const newTask = event.target.elements.task.value;
+    setTaskList(taskList => [newTask, ...taskList]);
+    event.target.reset();
+  }
+  function handleRemove(index) {
+    setTaskList(taskList.filter((_, i) => i !== index));
+  }
+
+  const taskItems = taskList.map((task, index) => (
+    <div key={index}>
+      <button className='button' onClick={() => handleRemove(index)}>
+
+      <p>{task}</p>
+      </button>
+    </div>
+  ));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label htmlFor="task">To do task</label>
+          <input 
+            type="text" 
+            id="task"
+            name="task"
+            required
+          />
+          <button type="submit">Submit</button>
+        </fieldset>
+      </form>
+      <div>
+        {taskItems}
+      </div>
     </div>
   );
 }
